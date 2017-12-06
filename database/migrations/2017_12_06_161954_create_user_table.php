2ece1790b1dc667dcaf8ea9d64f539450c4b986a
Men phors <menphorsqty@gmail.com>
2017-12-06 23:23:29 +0700

    admin-22: migrate table user

---
+++ database/migrations/2017_12_06_161954_create_user_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('name',50);
+            $table->string('first_name',50);
+            $table->string('last_name',50);
+            $table->enum('gender', ['m','f']);
+            $table->date('dob');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password',150);
+            $table->string('forgot_password',150);
+            $table->string('updated_password',150);
+            $table->string('flag_sec',150);
+            $table->dateTime('log_date');
+            $table->integer('role_id')->unsigned();
+            $table->integer('level_id')->unsigned();
+            $table->text('address');
+            $table->string('published');
+            $table->string('created_by');
+            $table->string('tel',25)->unique();
+            $table->timestamps();
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
