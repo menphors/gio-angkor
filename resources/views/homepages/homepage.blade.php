f5f530a376dfc7c0d62e970d0bc431cb1bd00dd1
Chanveasna2 <hchanveasna@gmail.com>
2017-12-13 21:37:49 +0700

    ID-001:Home Page (Page)

---
+++ resources/views/homepages/homepage.blade.php
@@ -0,0 +1,243 @@
+
+@extends('homepages.header')
+<div class="container-fluid " >
+  <div class="container">
+        <div class="row justify-content-between">
+                  <nav class="nav col colorback">
+                        <div class="col-lg-12">
+                            <a class="navbar-brand " href="#">
+                               <img src="images/cambodia.png" width="30px" height="20px"  >
+                            </a>
+                            <a class="navbar-brand" href="#">
+                              <img src="images/uk.png" width="30px" height="20px" >
+                            </a>
+                            <a class="navbar-brand" style=" font-size:12px;color: black;" href="#">Tel: 023 222 555</a>
+                        </div>
+                        
+                  </nav>
+
+                  <div class="nav" >
+                    <li class="nav-item ">
+                        <a class="nav-link active fa fa-cart-arrow-down text-dark" aria-hidden="true" href="#"> Card <span class="badge badge-pill badge-danger">1</span></a>
+                      </li>
+                      <li class="nav-item ">
+                        <a class="nav-link fa fa-heartbeat  text-dark" aria-hidden="true" href="#"> Wish List</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link fa fa-lock text-dark" aria-hidden="true" href="#"> Sign In</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link fa fa-users text-dark" aria-hidden="true" href="#"> Free Join</a>
+                      </li>
+                  </div>        
+          </div>                      
+    </div> 
+  </div>
+
+  <div class="container-fluid recolor " >
+    <div class="container" >
+                  
+      <nav class="navbar recolornav">
+        <div class="col-lg-3" >
+          <a class="navbar-brand" href="#">
+            <img src="images/logogio.png" width="200px" height="40px">
+          </a>
+        </div>
+                            
+        <div class="col-lg-9">
+          <div class="input-group ">                              
+              <input type="text" class="form-control" placeholder="Seach...">
+              <span class="input-group-btn  mr-3"> 
+                <button class="btn btn btn-secondary dropdown" data-toggle="dropdown" type="submit">All Categries</button>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>  
+                <button class="btn btn-danger" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+              </span>
+              <ul class="nav float-right " style="margin-right: -46px;">
+                <li class="nav-item border border-dark border-right-0">
+                  <a class="nav-link ">800 million</a>
+                </li>
+                <li class="nav-item border border-dark">
+                  <a class="nav-link">200.000 successful</a>
+                </li>
+              </ul>                
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="container">
+        <div class="row help">
+            <div class=" title_box col-lg-3 " >Categories</div>
+                <nav class="nav col-lg-9 " id="fonts">
+                    <a class="nav-link " id="grey" href="#">Export US</a>
+                    <a class="nav-link" id="grey" href="#">Customer consultant</a>
+                    <a class="nav-link" id="grey" href="#">Shoping guide</a>
+                    <a class="nav-link" id="grey" href="#">Customer protection</a>
+                    <a class="nav-link" id="grey" href="#">International transportation</a>
+                    <a class="nav-link" id="grey" href="#">Pricing & Checking size</a>
+                    <div class="linewidth"></div> -->                        
+                </nav>
+        </div>
+                    
+
+                    
+    </div>
+  </div>
+
+  <div class="container">
+        <div class="row">
+            <div class="col-md-3 recolors">
+                <ul class="nav">
+                    <li class="">
+                      <a class="" href="#"><i class="fa fa-home fa-fw"></i>Women's Clothing</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-list-alt fa-fw"></i>Man's Clothing</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-file-o fa-fw"></i>Phone & Accessories</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i>Computer & Office</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-table fa-fw"></i>Consumer Electonics</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-tasks fa-fw"></i>Jewelry&Watches</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-calendar fa-fw"></i>Home & Garden,Furniture</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-book fa-fw"></i>Bags & Shoes</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-pencil fa-fw"></i>Toys, Kids & Baby</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-cogs fa-fw"></i>Sports & Outdoors</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-calendar fa-fw"></i>Health & Beauty, Hair</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-book fa-fw"></i>Automobiles & Motorcycles</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-pencil fa-fw"></i>Home Improvement</a>
+                    </li>
+                    
+                </ul>
+            </div>
+                  <div class="col-md-7 well " >
+                      
+                      <div id="carouselExampleIndicators" style="right: 15px; width: 680px;"  class="carousel slide" data-ride="carousel">
+                          <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                          </ol>
+                        <div class="carousel-inner imagesize">
+                              <div class="carousel-item active">
+                                <img class="d-block w-100" src="images/12.jpg" alt="First slide">
+                              </div>
+                              <div class="carousel-item">
+                                <img class="d-block w-100" src="images/34.png" alt="Second slide">
+                              </div>
+                              <div class="carousel-item">
+                                <img class="d-block w-100" src="images/45.jpg" alt="Third slide">
+                              </div>
+                            
+                              <div class="carousel-item">
+                                <img class="d-block w-100" src="images/123321.png" alt="Four slide">
+                              </div>
+                              <div class="carousel-item">
+                                <img class="d-block w-100" src="images/aa.png" alt="Five slide">
+                              </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>
+
+                  </div>
+
+                  <div class="col-md-2 well">
+                      <a class="" href="#">
+                          <img src="images/170.png" width="175px" height="410px"  >
+                      </a>
+                  </div>
+
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row " style="padding-top: 30px;">
+          <div class="col-lg-3">
+            
+            <nav class="nav flex-column  " >
+              <a class="nav-link" id="fontss" href="#"><i class="fa fa-usd" aria-hidden="true"></i> GREATE VALUE</a>
+              <a class="nav-link" id="fontsss" href="#">
+              We offer competitive prices on our 100 million plus product range.</a>
+              
+            </nav>
+          </div>
+          <div class="col-lg-3">
+            <nav class="nav flex-column ">
+              <a class="nav-link" id="fontss" href="#"><i class="fa fa-shield" aria-hidden="true"></i> Shop with Confidence</a>
+              <a class="nav-link" id="fontsss" href="#">
+              Our Buyer Protection covers your purchase from click to delivery.</a>
+              
+            </nav>
+          </div>
+          <div class="col-lg-3">
+            <nav class="nav flex-column ">
+              <a class="nav-link" id="fontss" href="#"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Safe Payment</a>
+              <a class="nav-link" id="fontsss" href="#">
+              Pay with the world’s most popular and secure payment methods.</a>
+              
+            </nav>
+          </div>
+          <div class="col-lg-3">
+             <nav class="nav flex-column ">
+              <a class="nav-link" id="fontss" href="#"><i class="fa fa-truck" aria-hidden="true"></i> LOCAL DELIVERY</a>
+              <a class="nav-link" id="fontsss" href="#">
+              Cheap location shipping costs, Free delivery in Phnom Penh</a>
+              
+            </nav>
+          </div>
+        </div>
+      </div>
+
+   
+      
+
+
+      
+        
+        
+        @extends('homepages.footer')
+        @extends('homepages.womanclothing')
+        @extends('homepages.collection')
+
+        
+          
+
+      
+          
+        
+
+    
+
---
+++ resources/views/homepages/womanclothing.blade.php
@@ -0,0 +1,26 @@
+<div class="container" style="padding-top: 10px;">
+    <div class="row ">
+            <div>
+              <nav class="nav flex-column col-lg-3 float-left " >
+                  <a class="nav-link" id="fontssa" href="#">WOMEN'S CLOTHING</a>
+                  <div style="background-color: #e4e4e4; text-align: center;">
+                  <a class="nav-link"  href="#">Casual Dresses</a>
+                  <a class="nav-link"  href="#">Autumn Dresses</a>
+                  <a class="nav-link"  href="#">Coats & Jackets</a>
+                  <a class="nav-link"  href="#">Sweaters</a>
+                  <a class="nav-link"  href="#">Blouses & Shirts</a>
+                  <a class="nav-link"  href="#">T-Shirts</a>
+                  <a class="nav-link"  href="#">Sunglasses</a>
+                  <a class="nav-link"  href="#">Intimates&Sleepwear</a>
+                  <a class="nav-link"  href="#">Hair Accessories</a>
+                  </div>
+                </nav>                     
+                      <img src="images/300.png" alt="..." class="rounded float-left col-md-3" style="">
+                      <img src="images/250.png" alt="..." class="rounded float-right col-md-2" style="">
+                      <img src="images/400.png" alt="..." class="rounded float-right col-md-4" style="">
+                      <img src="images/250.png" alt="..." class="rounded float-right col-md-2" style=""> 
+                      <img src="images/250.png" alt="..." class="rounded float-right col-md-2" style="">
+                      <img src="images/250.png" alt="..." class="rounded float-right col-md-2" style="">
+            </div>
+    </div>
+</div>
