2140985510003ed060e58d97b6b772225aa43685
Men phors <menphorsqty@gmail.com>
2018-02-04 13:18:28 +0700

    Merge branch 'admin-32' of https://github.com/menphors/gio-angkor into admin-32
    # Conflicts:
    #	app/GroupCategory.php
    #	database/migrations/2017_12_04_114416_create_group_category_table.php
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,7 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'adminz'], function() {
     Route::resource('group-category','GroupCategoryController');
     Route::resource('category','CategoryController');
     Route::resource('subcategory','SubCategoryController');
+    Route::resource('tracking','TrackingsController');
+    Route::resource('promotions','PromotionsController');
+    Route::resource('discount','DiscountsController');
 });
