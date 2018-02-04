017210ae6b3bebb68194bc2967294f62a72ffd62
Men phors <menphorsqty@gmail.com>
2018-02-04 14:29:03 +0700

    Completed basic validation upload function

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -12,11 +12,17 @@ class UploadController extends Controller
 
     public  function saveUpload (Request $request) {
         $file = $request->file('file_upload');
+        //validation file image
+        $this->validate($request, [
+            'file_upload' => 'image|mimes:jpeg,bmp,png|size:5000'
+        ]);
 
         //$input['imagename'] = time().'.'.$image->getClientOriginalExtension();
+        //get original name
         $fileName = $file->getClientOriginalName();
+        //file distination
         $destinationPath = public_path('/uploads');
-
+        //move fie and name
         $file->move($destinationPath, $fileName);
 
     }
