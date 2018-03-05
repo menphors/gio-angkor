cfd0e6e247b86a2bbfd8bc259f3d1636af254b00
Men phors <menphorsqty@gmail.com>
2018-03-06 00:41:06 +0700

    gallery-main: Main Gallery

--- resources/views/admin/promotions/table.blade.php
+++ resources/views/admin/promotions/table.blade.php
@@ -43,11 +43,11 @@
             <td>{{$promotions->published}}</td>
             <td>
                 <a href="{{ route($route_prefix . '.edit', $promotions) }}" class="btn btn-sm btn-success btn-edit-row">
-                    <i class="fa fa-eye" aria-hidden="true"></i>&nbsp;&nbsp;Modify
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
                 </a>
 
                 <a href="javascript:void(0);" class="btn btn-sm btn-danger btn-delete-row">
-                    <i class="fa fa-times" aria-hidden="true"></i>&nbsp;&nbsp;Delete
+                    <i class="fa fa-times" aria-hidden="true"></i>
                 </a>
 
                 <form method="POST" class="form-delete-row" action="{!! route($route_prefix . '.destroy', $promotions) !!}" accept-charset="UTF-8">
