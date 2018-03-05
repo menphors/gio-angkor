cfd6df8a2b7023a4827a2f6dbe14f29d197e8cde
Men phors <menphorsqty@gmail.com>
2018-03-05 23:50:54 +0700

    gallery-main: update activity log

--- resources/views/admin/logactivities/table.blade.php
+++ resources/views/admin/logactivities/table.blade.php
@@ -9,34 +9,39 @@
     <thead>
     <tr>
         <th>LogID</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Date</th>
         <th>Status</th>
-        <th>Action</th>
+        <th>Login Date </th>
+        <th>Last Login date</th>
+        <th>Last Logout Date</th>
+        <th>Flag Login</th>
+        <th>Flag Logut</th>
+        <th>Session</th>
     </tr>
     <thead>
     <tbody>
     @foreach($crud_list as $logActivity)
         <tr>
             <td>{{$logActivity->id}}</td>
-            <td>{{$logActivity->quantity}}</td>
-            <td>{{$logActivity->price}}</td>
-            <td>{{$logActivity->date}}</td>
             <td>{{$logActivity->status}}</td>
+            <td>{{$logActivity->date}}</td>
+            <td>{{$logActivity->created_at}}</td>
+            <td>{{$logActivity->updated_at}}</td>
+            <td>{{$logActivity->flg_login}}</td>
+            <td>{{$logActivity->flg_logout}}</td>
+            <td>{{$logActivity->session}}</td>
             <td>
-                <a href="{{ route($route_prefix . '.edit', $logActivity) }}" class="btn btn-sm btn-success btn-edit-row">
-                    <i class="fa fa-eye" aria-hidden="true"></i>&nbsp;&nbsp;Modify
-                </a>
+                {{--<a href="{{ route($route_prefix . '.edit', $logActivity) }}" class="btn btn-sm btn-success btn-edit-row">--}}
+                    {{--<i class="fa fa-eye" aria-hidden="true"></i>&nbsp;&nbsp;Modify--}}
+                {{--</a>--}}
 
-                <a href="javascript:void(0);" class="btn btn-sm btn-danger btn-delete-row">
-                    <i class="fa fa-times" aria-hidden="true"></i>&nbsp;&nbsp;Delete
-                </a>
+                {{--<a href="javascript:void(0);" class="btn btn-sm btn-danger btn-delete-row">--}}
+                    {{--<i class="fa fa-times" aria-hidden="true"></i>&nbsp;&nbsp;Delete--}}
+                {{--</a>--}}
 
-                <form method="POST" class="form-delete-row" action="{!! route($route_prefix . '.destroy', $logActivity) !!}" accept-charset="UTF-8">
-                    {{ csrf_field() }}
-                    {{ method_field('DELETE') }}
-                </form>
+                {{--<form method="POST" class="form-delete-row" action="{!! route($route_prefix . '.destroy', $logActivity) !!}" accept-charset="UTF-8">--}}
+                    {{--{{ csrf_field() }}--}}
+                    {{--{{ method_field('DELETE') }}--}}
+                {{--</form>--}}
             </td>
         </tr>
     @endforeach
