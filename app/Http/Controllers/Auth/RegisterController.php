eb6b7a38481024e247bd6b7bdc733506f45af8b2
menphors <menphorsqty@gmail.com>
2018-08-26 21:21:10 +0700

    recorrect redirect after login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/frontend/dashboard-user';
+    protected $redirectTo = '/profile';
 
     /**
      * Create a new controller instance.
