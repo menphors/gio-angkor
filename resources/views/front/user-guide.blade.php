6249dce3ef7bc8c34f74d55d465012bbf24ca825
veasna <hchanveasna@gmail.com>
2018-08-26 17:04:42 +0700

    Done User Guide

--- resources/views/front/user-guide.blade.php
+++ resources/views/front/user-guide.blade.php
@@ -14,14 +14,75 @@
         <h1>User Guide</h1>
         <br>
         <h3>•   Register</h3>
-        <p>Input:</p>
-            <b>Username</b>
-            <p>Gender</p>
-            <p>Date of Birth</p>
-            <p>Telephone</p>
-            <p>Email</p>
-            <p>Password</p>
-        <p>Click Register</p>
+        <p>Input:<br>
+            Username<br>
+            Gender<br>
+            Date of Birth<br>
+            Telephone<br>
+            Email<br>
+            Password<br>    
+            Click Register
+        </p>
+        <img src="../picuserguide/register.PNG"/>
+
+        <br>
+        <h3>•   Log In</h3>
+        <p>Input:<br>
+            Email<br>
+            Password<br>  
+            Click Sing In
+        </p>
+        <img src="../picuserguide/login.PNG"/>
+
+         <br>
+        <h3>•   View Product Detail</h3>
+        <p> Search Product <br>
+            Click on View Detail to view Product Description
+
+        </p>
+        <img src="../picuserguide/product.PNG"/>
+
+        <br>
+        <h3>•   Product Detail</h3>
+        <p>Click <b>ADD TO CARD</b> to order product
+
+        </p>
+        <img src="../picuserguide/productdetail.PNG"/>
+        
+        <br>
+        <h3>•   Add to Card</h3>
+        <p>Click <b>Checkout</b> to input Order informaiton
+
+        </p>
+        <img src="../picuserguide/add-to-card.PNG"/>
+
+        <br>
+        <h3>•   Checkout</h3>
+        <p>Input:<br>
+            First Name<br>
+            Last Name<br>
+            Phone<br>
+            Email<br>
+            Click <b>Next</b>
+        </p>
+        <img src="../picuserguide/checkout.PNG"/>
+
+        <br>
+        <h3>•   Process Order</h3>
+        <img src="../picuserguide/processorder.PNG"/>
+
+        <br>
+        <h3>•   Payment</h3>
+        <p>Input:<br>
+            Payment Type<br>
+            Payment Code<br>
+            Click <b>Place Order<b> to order Successful
+        </p>
+        <img src="../picuserguide/payment.PNG"/>
+
+        <br>
+        <h3>•   Order Successful</h3>
+        <img src="../picuserguide/ordersuccess.PNG"/>
 
     </div>
 </div>
